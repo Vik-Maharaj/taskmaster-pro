3481ab1e2df277b9adf11950b2314322047301bf
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-03-16 20:24:47 -0400

    Add interval function to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -236,6 +236,14 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
+
+
 loadTasks();
 
 
